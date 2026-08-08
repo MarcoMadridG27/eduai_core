ad9354662736569eaa0529ef9c3b133d8c08f166
Marco <marco@MacBook-Air-de-Marco.local>
2026-08-08 13:24:36 -0500

    fix: CORS middleware allow_origin_regex to fix browser CORS block

--- main.py
+++ main.py
@@ -73,10 +73,9 @@ async def startup():
 
 
 # --- CORS Middleware ---
-origins = ["*"]  # Ajusta en producción
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origin_regex=r"https?://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
